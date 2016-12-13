8e74aa4840533e38a1c68aa364badd1bcd86ab12
Mardari Igor <igor.mardari96@gmail.com>
2016-12-13 16:08:36 +0200

    Update wp-bootstrap-navwalker.php

--- wp-bootstrap-navwalker.php
+++ wp-bootstrap-navwalker.php
@@ -50,7 +50,7 @@ function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 	 
 	$output .= $indent . '<li' . $id . $value . $class_names . $li_attributes . '>';
 	 
-	//$attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) .'"' : '';
+	$attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) .'"' : '';
 	$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) .'"' : '';
 	$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) .'"' : '';
 	$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) .'"' : '';
